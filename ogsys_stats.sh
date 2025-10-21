bbb15883b278b8555c0533dcc4974b992dfd900c
cameronloflin-png <cameronloflin@gmail.com>
2025-10-21 11:56:58 -0600

    Update ogsys_stats.sh

--- ogsys_stats.sh
+++ ogsys_stats.sh
@@ -4,73 +4,47 @@
 #******* OUTPUT SYSTEM STATS ********#
 #************************************#
 
-# Make a directory called System_Stats and change directories to that directory
-mkdir System_Stats
+# Create and enter System_Stats directory
+mkdir -p System_Stats
 cd System_Stats || exit
-# Try to make the output for each file look as neat and organized as you can.
 
-# Output the following information to a file called kernel
-touch kernel.txt
-    # 1. Kernel Name
-   {
-    uname
-    # 2. Kernel Release
-    sudo lshw -short
-    # 3. Kernel Version
-    sudo lshw -html > lshw.html 
-    # 4. The operating system
-    lscpu
-    } > kernel
+# Output kernel info
+{
+  uname
+  sudo lshw -short
+  sudo lshw -html > lshw.html
+  lscpu
+} > kernel.txt
 
+# Output network info
+{
+  netstat
+} > network.txt
 
-# Output the following information to a file called network
-touch network.txt
-    # 1. Do not print any serial numbers or sensitive information related to the system
-    # 2. All network interfaces
-    {
-    netstat
-    } > network
-    
-# Output the following information to a file called disk.html
-touch disk.html
-    # 1. All disks
-    {
-    df -h
-    } > disk.html
-    # 2. The output should include html tags, i.e. <html></html>
-    
+# Output disk info
+{
+  echo "<html>"
+  df -h
+  echo "</html>"
+} > disk.html
 
-
-# Output the following information to a file called cpu
-    # 1. The first five lines of the command lscpu
-    # 2. The last 12 lines of the command lscpu
-  {  
+# Output CPU info
+{
   lscpu | head -n 5
   echo
   lscpu | tail -n 12
-} > cpu
+} > cpu.txt
 
+# Output block device info
+{
+  lsblk -o NAME,SIZE,TYPE -a --ascii
+} > block_dev.txt
 
-# Output the following information to a file called block_dev
-    # 1. Only the name, size, and type of the block devices
-    # 2. The output should use ascii characters for any tree formatting
-    {
-    lsblk -o NAME,SIZE,TYPE -a --ascii 
-   } > block_dev
-
-    {
+# Output SATA device info
+{
   lsblk -o NAME,SIZE,TYPE,TRAN --ascii | grep 'sata'
 
   for dev in $(lsblk -d -o NAME,TRAN | awk '$2 == "sata" {print $1}'); do
     udevadm info --query=all --name="/dev/$dev"
   done
-} > sata
-
-    
-    
-
-
-
-
-
-    
+} > sata.txt
